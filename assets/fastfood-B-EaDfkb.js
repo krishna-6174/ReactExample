6e5b36c9ec14696c157350165edbc96cfb4a4b6a
Krishna Bandla <gopalakrishnabandla2003@gmail.com>
2025-09-12 19:42:04 +0530

    Updates

--- assets/fastfood-B-EaDfkb.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Cf_GWrgA.js";function n(){return o.jsx(o.Fragment,{children:"Fast Food Component"})}export{n as default};
---
+++ assets/fastfood-Dt-2TdwS.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CK9q3P3p.js";function n(){return o.jsx(o.Fragment,{children:"Fast Food Component"})}export{n as default};
